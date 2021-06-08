ac914429309409191a107bf089ed071ec7d5bee5
Sultan-Kathat <64056864+Sultan-Kathat@users.noreply.github.com>
2021-06-08 11:06:06 +0530

    fonts changed

--- foodhub/settings.py
+++ foodhub/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-4o99w^a0od(^$2$&if6dv5m#^z1mhld$=5edaqyfn179a-y-bf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -131,7 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,  'static')
+#STATIC_ROOT = os.path.join(BASE_DIR,  'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
